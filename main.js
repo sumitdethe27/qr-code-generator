5ffb12483c7c08628c455a464089cb859b089cc3
unknown <milinddethe6@gmail.com>
2023-05-20 10:10:11 +0530

    styles added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const qr_gen="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 let btn=document.querySelector(".btn");
 btn.addEventListener("click",()=>{
     let url=document.querySelector(".address").value;
-    
+    if(url.length==0)return alert("Input a link")
     if(url.startsWith("http")){
         console.log("true")
         url=url.slice(12)
@@ -12,6 +12,7 @@ btn.addEventListener("click",()=>{
        url= url.slice(4)
     }
 let ans=qr_gen+url;
-window.open(ans,"_blanck")
+window.open(ans,"_blanck");
+
 
 })
